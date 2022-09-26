84219c76471518030c95b6b3a2e89575ccf970ad
RubenSystems <ruben.t.j@icloud.com>
2022-09-26 10:50:05 +0100

    first try

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    
+    for i in items:
+        item = str(i)
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
+
     return frequencies
